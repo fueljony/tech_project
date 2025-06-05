0b42f74f9466fba83c96b53065d5de0b1e990acb
Jon Sanchez <fueljony@gmail.com>
2025-06-05 12:48:59 -0600

    changed port

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default defineConfig(({ mode }) => {
         },
         server: {
             host: "127.0.0.1",
-            port: 3000,
+            port: 3001,
             strictPort: true,
             hmr: {
                 host: "127.0.0.1",
