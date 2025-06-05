515e82c792f56c65292917bb9eb685cee62dfa93
GerardoRebo <81606616+GerardoRebo@users.noreply.github.com>
2025-06-05 11:59:53 -0600

    survyes

--- resources/js/surveys/views/Index.vue
+++ resources/js/surveys/views/Index.vue
@@ -7,14 +7,29 @@ surveyStore.loadInitialData();
 const tableHeaders = [
   { text: 'Id', value: 'id', sortable: false, align: 'start' },
   { text: 'Title', value: 'name', sortable: false, align: 'start' },
-  { text: 'Options', value: 'options', sortable: false, align: 'start' },
+  { text: 'Actions', value: 'actions', sortable: false, align: 'start' },
 ];
 </script>
 
 <template>
-  <div >
+  <div>
     {{ surveys }}
-    <v-data-table :headers="tableHeaders" :items="surveys"></v-data-table>
+    <v-data-table :headers="tableHeaders" :items="surveys">
+      <template v-slot:item.actions="{ item }">
+        <RouterLink :to="{
+          name: 'survey.show',
+          params: { id: item.id }
+        }">
+          <v-btn color="primary" outlined @click="surveyStore.editSurvey(item.id)" class="mr-3" small>View</v-btn>
+        </RouterLink>
+        <RouterLink :to="{
+          name: 'survey.edit',
+          params: { id: item.id }
+        }">
+          <v-btn @click="surveyStore.editSurvey(item.id)" small>Edit</v-btn>
+        </RouterLink>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
